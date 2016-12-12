a6e9b2a934dd7f3214b4cfcae0bc08fea02e0743
Thiago Barcala <thiago.barcala@gmail.com>
2016-12-12 08:15:19 +0100

    Improve ID retrieval for WG-Gesucht

--- trackers/wg-gesucht.js
+++ trackers/wg-gesucht.js
@@ -4,10 +4,10 @@ module.exports = {
     adapter: function ($) {
         var items = [];
         $('tr.listenansicht0,tr.listenansicht1').each(function () {
-            var adId = $(this).attr('adid');
+            var adUrl = $(this).attr('adid');
             items.push({
-                id: adId,
-                url: `http://www.wg-gesucht.de/${adId}`,
+                id: adUrl.replace(/^.*\.([0-9]+)\.html/, "$1"),
+                url: `http://www.wg-gesucht.de/${adUrl}`,
                 title: $(this).find('td').map(function () {
                     return $(this).text().trim() 
                 }).toArray().filter(x => x).join(', ')
